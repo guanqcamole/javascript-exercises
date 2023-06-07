7590763e3f12fee2ae6fbfd34724c9f42129e949
Quang Pham <quangpham4071@gmail.com>
2023-06-06 21:45:58 -0500

    completed 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1, num2) {
     //return "ERROR" if either number is a non-number
-    if ((typeof num1 != 'number' && typeof num2 != 'number')) return "ERROR";
+    if (typeof num1 != 'number' || typeof num2 != 'number') return "ERROR";
 
     //return "ERROR" if either number is negative
     if (num1 < 0 || num2 < 0) return "ERROR";
