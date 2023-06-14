335cd572cf64696a1a07fe9e1266e194d54f0429
Quang Pham <quangpham4071@gmail.com>
2023-06-14 13:27:47 -0500

    completed all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const curYear = 2023
+    let maxAge = 0;
+    let oldest = '';
+    for (person of people) {
+        let yearOfDeath = person.yearOfDeath;
+        console.log(yearOfDeath);
+        if (typeof yearOfDeath === 'undefined') {
+            yearOfDeath = curYear;
+        }
 
+        const age = yearOfDeath - person.yearOfBirth;
+
+        if (age > maxAge) {
+            maxAge = age;
+            console.log(maxAge);
+            oldest = person;
+        }
+    };
+
+    return oldest;
 };
 
 // Do not edit below this line
